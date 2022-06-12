b543d23e9f643945ecb2f7cec4b797b94f6bd4fa
Paweł Biegun <biegunpawel900@gmail.com>
2022-06-12 09:48:52 +0200

    add delete_user to user_service

--- services/user_service.py
+++ services/user_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 import passlib.hash as hash
 import schemas.user_schema as user_schema
 import models.user_model as user_model
+from fastapi import HTTPException
 
 
 def get_user(db: Session, user_id: int) -> user_model.User:
@@ -28,3 +29,13 @@ def create_user(db: Session, user: user_schema.UserCreate) -> user_model.User:
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def delete_user(db: Session, user_id: int):
+    """
+    Delete and existing user
+    """
+    user = db.query(user_model.User).filter(user_model.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=400, detail=f"A user with id: {user_id} doesn't exist")
+    db.delete(user)
+    db.commit()
--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -1,7 +1,10 @@
+import pytest
+from fastapi import HTTPException
 from services import user_service
 from utils import testing_utils
 from utils.database_utils import SessionLocal
 from schemas.user_schema import UserCreate
+from models import user_model
 
 def test_get_user():
     user = testing_utils.create_user()
@@ -33,3 +36,24 @@ def test_create_user():
     assert user_from_db.first_name == user_data.first_name
     assert user_from_db.last_name == user_data.last_name
     assert user_from_db.hashed_password != user_data.password
+
+def test_delete_user():
+    user = testing_utils.create_user()
+    db = SessionLocal()
+    num_users_before_delete = db.query(user_model.User).count()
+    user_service.delete_user(db=db, user_id=user.id)
+    num_users_after_delete = db.query(user_model.User).count()
+    db.close()
+    assert num_users_after_delete - num_users_before_delete == -1
+    assert user_service.get_user(db=db, user_id=user.id) == None 
+    assert user_service.get_user_by_email(db=db, email=user.email) == None
+
+def test_delete_non_existant_user():
+    db = SessionLocal()
+    num_users_before_delete = db.query(user_model.User).count()
+    with pytest.raises(HTTPException):
+        #User with this id cannot exist in the db
+        user_service.delete_user(db=db, user_id=-200)
+    num_users_after_delete = db.query(user_model.User).count()
+    assert num_users_after_delete - num_users_before_delete == 0
+    db.close()
